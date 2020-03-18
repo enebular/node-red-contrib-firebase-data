f65b0989b9869cd2e62b50205308c720f5c0d925
taminhhienmor <56866116+taminhhienmor@users.noreply.github.com>
2020-03-18 16:25:05 +0900

    update certificate

--- source/addFirebase.js
+++ source/addFirebase.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var {google} = require("googleapis");
 module.exports = function (RED) {
   "use strict";
 
@@ -11,13 +12,10 @@ module.exports = function (RED) {
     var methodValue = n.method
     var childPath = n.childpath || ""
     var jsonPath = ".json"
-    // var private_key = n.private_key;
-    // var client_email = n.client_email
-    // var firebaseCertificate = RED.nodes.getNode(n.firebaseCertificate);
+    var firebaseCertificate = RED.nodes.getNode(n.firebaseCertificate);
+    var url_params = "";    
 
-    node.on("input", function (msg) {    
-      
-      console.log("newObj1", newObj); 
+    node.on("input", function (msg) {     
       if(typeof msg.payload != 'number') {
         newObj = msg.payload
       }
@@ -25,63 +23,84 @@ module.exports = function (RED) {
       if (typeof newObj != 'object') {
         newObj = JSON.parse(newObj)
       };
+
       if (methodValue == "msg.method" || methodValue == "") {
         methodValue = msg.method
-      };
+      };  
 
-      console.log("newObj", newObj);      
+      if (firebaseCertificate.loginType == "jwt") {
+        // Define the required scopes.
+        var scopes = [
+          "https://www.googleapis.com/auth/userinfo.email",
+          "https://www.googleapis.com/auth/firebase.database"
+        ];
 
+        var decoder = firebaseCertificate.secret.replace(/\\n/g,"\n")
+        var jwtClient = new google.auth.JWT(firebaseCertificate.email, null, decoder, scopes);
 
-      // var {google} = require("googleapis");
+        // Use the JWT client to generate an access token.
+        jwtClient.authorize(function(error, tokens) {
+          if (error) {
+            node.error(error, {});
+            node.status({fill: "red", shape: "ring", text: "Error making request to generate access token"});
+            return;
+          } else if (tokens.access_token === null) {
+            node.error(error, {});
+            node.status({fill: "red", shape: "ring", text: "Provided service account does not have permission to generate access tokens"});
+            return;
+          } else {
+            var accessToken = tokens.access_token;
+            url_params = "https://" + firebase + ".firebaseio.com/" + childPath + jsonPath+"?access_token=" + accessToken
+            var opts = {
+              method: methodValue,
+              url: url_params,
+              body: JSON.stringify(newObj)
+            };
 
-      // Define the required scopes.
-      // var scopes = [
-      //   "https://www.googleapis.com/auth/userinfo.email",
-      //   "https://www.googleapis.com/auth/firebase.database"
-      // ];
+            request(opts, function (error, response, body) {
+              if (error) {
+                node.error(error, {});
+                node.status({fill: "red", shape: "ring", text: "failed"});
+                return;
+              } else {
+                if (methodValue == "delete") {
+                  msg.payload = "Delete success!"
+                  node.send(msg);
+                } else {
+                  msg.payload =  JSON.parse(body);
+                  node.send(msg);
+                }
+              }    
+            })   
+          }
+        });        
+      } else {
+        url_params = "https://" + firebase + ".firebaseio.com/" + childPath + jsonPath
+        var opts = {
+          method: methodValue,
+          url: url_params,
+          body: JSON.stringify(newObj)
+        };
 
-      // var decoder = private_key.replace(/\\n/g,"\n")
-      // var jwtClient = new google.auth.JWT(client_email, null, decoder, scopes);      
-
-      // // Use the JWT client to generate an access token.
-      // jwtClient.authorize(function(error, tokens) {
-      //   if (error) {
-      //     console.log("Error making request to generate access token:", error);
-      //   } else if (tokens.access_token === null) {
-      //     console.log("Provided service account does not have permission to generate access tokens");
-      //   } else {
-      // var accessToken = tokens.access_token;
-
-      var opts = {
-        method: methodValue,
-        // url: "https://" + firebase + ".firebaseio.com/" + childPath + jsonPath+"?access_token=" +accessToken,
-        url: "https://" + firebase + ".firebaseio.com/" + childPath + jsonPath,
-        body: JSON.stringify(newObj)
-      };
-
-      request(opts, function (error, response, body) {
-        if (error) {
-          node.error(error, {});
-          node.status({
-            fill: "red",
-            shape: "ring",
-            text: "failed"
-          });
-          return;
-        }
+        request(opts, function (error, response, body) {
+          if (error) {
+            node.error(error, {});
+            node.status({fill: "red", shape: "ring", text: "failed"});
+            return;
+          } else {
+            if (methodValue == "delete") {
+              msg.payload = "Delete success!"
+              node.send(msg);
+            } else {
+              msg.payload =  JSON.parse(body);
+              node.send(msg);
+            }
+          }    
+        })
         
-
-        if (methodValue == "delete") {
-          msg.payload = "Delete success!"
-        } else {
-          msg.payload = JSON.parse(body)
-        }
-
-        node.send(msg);
-      })
-      //   }
-      // });        
+      }
     });
   }
+
   RED.nodes.registerType("addFirebase", addFirebase);
 };
--- source/firebaseCertificate.js
+++ source/firebaseCertificate.js
@@ -1,5 +1,3 @@
-var request = require('request');
-var firebase = require('firebase');
 module.exports = function(RED) {
   "use strict";
   function firebaseCertificate(n) {
@@ -9,23 +7,15 @@ module.exports = function(RED) {
 
     this.firebaseurl = "https://" + n.firebaseurl + ".firebaseio.com";
     this.loginType = n.loginType;
-    this.uid = this.credentials.uid;
     this.secret = this.credentials.secret;
     this.email = this.credentials.email;
-    this.password = this.credentials.password;
-    this.jwtClaims = JSON.parse(this.credentials.jwtClaims != undefined ? this.credentials.jwtClaims : "[]");
-    // console.log("firebaseurl", this.firebaseurl);
-    // console.log("email", this.email);
     
   }
   RED.nodes.registerType("firebaseCertificate",firebaseCertificate, {
     credentials: {
         loginType: {type: 'text'},
-        uid: {type: 'text'},
         secret: {type: 'password'},
-        email: {type: 'text'},
-        password: {type: 'password'},
-        jwtClaims: {type: 'text'}
+        email: {type: 'text'}
     }
   });
 };

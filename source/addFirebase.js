fe537a130cffaa8130b890102ccd45d08309700e
taminhhienmor <56866116+taminhhienmor@users.noreply.github.com>
2020-04-08 18:29:51 +0900

    update feature rename childPath

--- source/addFirebase.js
+++ source/addFirebase.js
@@ -1,106 +1,181 @@
 var request = require('request');
 var {google} = require("googleapis");
 module.exports = function (RED) {
-  "use strict";
+    "use strict";
 
-  function addFirebase(n) {
-    RED.nodes.createNode(this, n);
-    this.box = RED.nodes.getNode(n.box);
-    var node = this;
-    
-    var newObj = n.data || {}
-    var methodValue = n.method
-    var childPath = n.childpath || ""
-    var jsonPath = ".json"
-    var firebaseCertificate = RED.nodes.getNode(n.firebaseCertificate);
-    var url_params = "";    
+    function addFirebase(n) {
+        RED.nodes.createNode(this, n);
+        this.box = RED.nodes.getNode(n.box);
+        var node = this;
+        
+        var newObj = n.data || {}
 
-    node.on("input", function (msg) {     
-      if(typeof msg.payload != 'number') {
-        newObj = msg.payload
-      }
+        var methodValue = n.method
+        var newchildpath = n.newchildpath
+        var childPathProperty = n.childpath || ""
+        var propertyType = n.propertyType || "msg";
+        var globalContext = this.context().global;
+        var flowContext = this.context().flow;
 
-      if (typeof newObj != 'object') {
-        newObj = JSON.parse(newObj)
-      };
+        var firebaseCertificate = RED.nodes.getNode(n.firebaseCertificate);
+        var jsonPath = ".json"
+        var url_params = "";
 
-      if (methodValue == "msg.method" || methodValue == "") {
-        methodValue = msg.method
-      };  
+        node.on("input", function (msg) {
+            node.status({});
+            
+            // select method 
+            switch (methodValue) {
+                case "set":
+                    methodValue = "PUT"
+                    break;
+                case "push":
+                    methodValue = "POST"
+                    break;
+                case "update":
+                    methodValue = "PATCH"
+                    break;
+                case "remove":
+                    methodValue = "DELETE"
+                    break;
+                default:
+                    methodValue = methodValue
+                    break;
+            }
 
-      if (firebaseCertificate.loginType == "jwt") {
-        // Define the required scopes.
-        var scopes = [
-          "https://www.googleapis.com/auth/userinfo.email",
-          "https://www.googleapis.com/auth/firebase.database"
-        ];
+            // select childPath
+            var childPath = "";
+            switch (propertyType) {
+                case "str":
+                    childPath = childPathProperty
+                    break;
+                case "msg":
+                    childPath = msg[childPathProperty]
+                    break;
+                case "flow":
+                    childPath = flowContext.get(childPathProperty)
+                    break;
+                case "global":
+                    childPath = globalContext.get(childPathProperty)
+                    break;
+                default:
+                    childPath = childPathProperty
+                    break;
+            }
 
-        var decoder = firebaseCertificate.secret.replace(/\\n/g,"\n")
-        var jwtClient = new google.auth.JWT(firebaseCertificate.email, null, decoder, scopes);
+            if (methodValue == "reanameChildPath") {
+                childPath = newchildpath
+            } else if (methodValue == "setPriority" || methodValue == "setWithPriority") {
+                methodValue = "put"
+            } else if (methodValue == "msg.method" || methodValue == "") {
+                methodValue = msg.method
+            };
+            
+            if(typeof msg.payload != 'number') {
+                newObj = msg.payload
+            }
 
-        // Use the JWT client to generate an access token.
-        jwtClient.authorize(function(error, tokens) {
-          if (error) {
-            node.error(error, {});
-            node.status({fill: "red", shape: "ring", text: "Error making request to generate access token"});
-            return;
-          } else if (tokens.access_token === null) {
-            node.error(error, {});
-            node.status({fill: "red", shape: "ring", text: "Provided service account does not have permission to generate access tokens"});
-            return;
-          } else {
-            var accessToken = tokens.access_token;
-            url_params = firebaseCertificate.firebaseurl + childPath + jsonPath+"?access_token=" + accessToken
-            var opts = {
-              method: methodValue,
-              url: url_params,
-              body: JSON.stringify(newObj)
+            if (typeof newObj != 'object') {
+                newObj = JSON.parse(newObj)
             };
 
-            request(opts, function (error, response, body) {
-              if (error) {
-                node.error(error, {});
-                node.status({fill: "red", shape: "ring", text: "failed"});
-                return;
-              } else {
-                if (methodValue == "delete") {
-                  msg.payload = "Delete success!"
-                  node.send(msg);
-                } else {
-                  msg.payload =  JSON.parse(body);
-                  node.send(msg);
-                }
-              }    
-            })   
-          }
-        });        
-      } else {
-        url_params = firebaseCertificate.firebaseurl + childPath + jsonPath
-        var opts = {
-          method: methodValue,
-          url: url_params,
-          body: JSON.stringify(newObj)
-        };
+            if (firebaseCertificate.loginType == "jwt") {
+                // Define the required scopes.
+                var scopes = [
+                    "https://www.googleapis.com/auth/userinfo.email",
+                    "https://www.googleapis.com/auth/firebase.database"
+                ];
 
-        request(opts, function (error, response, body) {
-          if (error) {
-            node.error(error, {});
-            node.status({fill: "red", shape: "ring", text: "failed"});
-            return;
-          } else {
-            if (methodValue == "delete") {
-              msg.payload = "Delete success!"
-              node.send(msg);
+                var decoder = firebaseCertificate.secret.replace(/\\n/g,"\n")
+                var jwtClient = new google.auth.JWT(firebaseCertificate.email, null, decoder, scopes);
+
+                // Use the JWT client to generate an access token.
+                jwtClient.authorize(function(error, tokens) {
+                    if (error) {
+                        node.error(error, {});
+                        node.status({fill: "red", shape: "ring", text: "Error making request to generate access token"});
+                        return;
+                    } else if (tokens.access_token === null) {
+                        node.error(error, {});
+                        node.status({fill: "red", shape: "ring", text: "Provided service account does not have permission to generate access tokens"});
+                        return;
+                    } else {
+                        var accessToken = tokens.access_token;
+                        url_params = firebaseCertificate.firebaseurl + childPath + jsonPath+"?access_token=" + accessToken
+                        
+                    }
+                });        
             } else {
-              msg.payload =  JSON.parse(body);
-              node.send(msg);
+                url_params = firebaseCertificate.firebaseurl + childPath + jsonPath
             }
-          }    
-        })
-        
-      }
-    });
-  }
+            if (methodValue == "reanameChildPath") {
+                var optsGet = {
+                    method: "GET",
+                    url: url_params
+                };
+                request(optsGet, function (errorGet, response, bodyGet) {
+                    if (errorGet) {
+                        node.error(errorGet, {});
+                        node.status({fill: "red", shape: "ring", text: "failed"});
+                        return;
+                    } else {
+                        var dataClone = JSON.parse(bodyGet); 
+                        var optsPut = {
+                            method: "PUT",
+                            url: url_params,
+                            body: JSON.stringify(dataClone)
+                        };
+
+                        request(optsPut, function (errorPut, response, bodyPut) {
+                            if (errorPut) {
+                                node.error(errorPut, {});
+                                node.status({fill: "red", shape: "ring", text: "failed"});
+                                return;
+                            } else {
+                                var optsDelete = {
+                                    method: "DELETE",
+                                    url: url_params
+                                };
+    
+                                request(optsDelete, function (errorDelete, response, bodyDelete) {
+                                    if (errorDelete) {
+                                        node.error(errorDelete, {});
+                                        node.status({fill: "red", shape: "ring", text: "failed"});
+                                        return;
+                                    } else {
+                                        msg.payload = JSON.parse(bodyPut);
+                                        node.send(msg);
+                                    }
+                                })
+                            }    
+                        })
+                    }    
+                })
+            } else {
+                var opts = {
+                    method: methodValue,
+                    url: url_params,
+                    body: JSON.stringify(newObj)
+                };
+
+                request(opts, function (error, response, body) {
+                    if (error) {
+                        node.error(error, {});
+                        node.status({fill: "red", shape: "ring", text: "failed"});
+                        return;
+                    } else {
+                        if (methodValue == "delete") {
+                            msg.payload = "Delete success!"
+                            node.send(msg);
+                        } else {
+                            msg.payload =  JSON.parse(body);
+                            node.send(msg);
+                        }
+                    }    
+                })
+            }
+        });
+    }
 
   RED.nodes.registerType("addFirebase", addFirebase);
 };
